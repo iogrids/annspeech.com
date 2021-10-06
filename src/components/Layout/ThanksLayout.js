05f16b2520b900877ed731c5d77013bdd1806b26
iogrids <iogrids1@gmail.com>
2021-10-07 04:27:59 +0530

    modify thanks layout page

--- src/components/Layout/ThanksLayout.js
+++ src/components/Layout/ThanksLayout.js
@@ -21,78 +21,29 @@ const useStyles = makeStyles((theme) => ({
 const ThanksLayout = ({ children }) => {
   const classes = useStyles();
   return (
-    <div className="container">
-      <Head>
-        <title>Ann Speech</title>
-        <meta name="description" content="online speech therapy" />
-        <meta name="title" content="Online speech therapy" />
-        <meta
-          name="description"
-          content="Handles conditions such as Speech Delay, Unclear speech, Reading writing difficulties and other communication difficulties in Children and Adults!"
-        />
-        <meta
-          name="keywords"
-          content="online speech therapy, speech therapy, speech therapy for kids, speech therapy for adults, speech therapy for children"
-        />
-        <meta name="robots" content="index, follow" />
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="language" content="English"></meta>
-        <link rel="icon" href="/favicon.ico" />
-
-        <script
-          async
-          src="https://track.annspeech.com/conversion.gif?cid=OPTIONAL&payout=OPTIONAL&txid=OPTIONAL"
-        />
-
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-944956101"
-        />
-
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'AW-944956101', { page_path: window.location.pathname });
-            `,
-          }}
-        />
-
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              gtag('event', 'conversion', {'send_to': 'AW-944956101/XNRJCJKRs8sCEMXFy8ID'});
-            `,
-          }}
-        />
-      </Head>
-      <Container className={classes.mainDiv} maxWidth="lg">
-        <Menu />
-        <Typography
-          className={classes.title}
-          variant="h4"
-          gutterBottom
-          component="div"
-        >
-          THANK YOU!
-        </Typography>
-        <Typography
-          className={classes.body}
-          variant="h5"
-          gutterBottom
-          component="p"
-        >
-          In response to your inquiry, I have send the necessary information to
-          your email. If you cannot see it in your inbox, kindly check your
-          promotions folder.
-        </Typography>
-      </Container>
-
+    <Container className={classes.mainDiv} maxWidth="lg">
+      <Menu />
+      <Typography
+        className={classes.title}
+        variant="h4"
+        gutterBottom
+        component="div"
+      >
+        THANK YOU!
+      </Typography>
+      <Typography
+        className={classes.body}
+        variant="h5"
+        gutterBottom
+        component="p"
+      >
+        In response to your inquiry, I have send the necessary information to
+        your email. If you cannot see it in your inbox, kindly check your
+        promotions folder.
+      </Typography>
       <Testimonials />
       <Footer />
-    </div>
+    </Container>
   );
 };
 
