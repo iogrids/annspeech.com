e525565556020ac7308e602ac8d070863b64529b
iogrids <iogrids1@gmail.com>
2021-10-07 00:53:48 +0530

    removed theme provider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,4 @@
 import { useEffect } from 'react';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from '../../theme';
-import CssBaseline from '@mui/material/CssBaseline';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -10,13 +7,7 @@ function MyApp({ Component, pageProps }) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
   }, []);
-  return (
-    <ThemeProvider theme={theme}>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
