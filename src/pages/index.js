ab22c7442c45f5fc7baa421efb1214f336d29222
iogrids <iogrids1@gmail.com>
2021-10-10 12:35:43 +0530

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,26 @@
 import Layout from '../components/Layout/Layout';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  const { query } = useRouter();
+  let headline;
+  const toTitleCase = (str) => {
+    return str.replace(/\w\S*/g, function (txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  };
+  if (query.headline) {
+    headline = toTitleCase(query.headline);
+  } else {
+    headline = 'Looking for an Online Speech Therapist?';
+  }
   return (
     <main>
       <Head>
         <title>annspech.com</title>
 
-        <meta name="description" content="online speech therapy" />
+        <meta name="description" content={headline} />
       </Head>
 
       <Layout></Layout>
