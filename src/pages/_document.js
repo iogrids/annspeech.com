114d54ec922e6fa97c6eb90be294b7efebf41e55
iogrids <iogrids1@gmail.com>
2021-10-29 14:08:27 +0530

    updates

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,32 +6,9 @@ import React from 'react';
 export default class MyDocument extends Component {
   render() {
     return (
-      <Html lang="en" class="notranslate" translate="no">
+      <Html lang="en" className="notranslate" translate="no">
         <Head>
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-          {/* bemob tag start */}
-          <link
-            href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-          {/* bemob tag end */}
-
-          {/* Google Analytics tag start */}
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-DFRW4CQGQW"
-          />
-
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-DFRW4CQGQW');`,
-            }}
-          />
-          {/* Google Analytics tag end */}
 
           {/* Google Adwords tag start */}
           <script
@@ -67,6 +44,18 @@ export default class MyDocument extends Component {
         <body>
           <Main />
           <NextScript />
+          {/* Bemob analytics start */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `!(function () {
+                var a = document.createElement("script");
+                (a.type = "text/javascript"), (a.async = !0), (a.src = "https://track.annspeech.com/landing/b14f34d4-5810-4a26-825a-70f07f9e1d78?callback=REPLACE&rule=REPLACE&path=REPLACE&landing=REPLACE&" + window.location.search.substring(1));
+                var b = document.getElementsByTagName("script")[0];
+                b.parentNode.insertBefore(a, b);
+              })();`,
+            }}
+          />
+          {/* Bemob analytics end */}
         </body>
       </Html>
     );
---
+++ src/pages/speech-therapy-down-syndrome.js
@@ -0,0 +1,97 @@
+import Head from 'next/head';
+import Grid from '@mui/material/Grid';
+import { makeStyles } from '@material-ui/core';
+import Container from '@mui/material/Container';
+import Menu from '../components/Menu/Menu';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Form from '../components/Form/Form';
+import Content from '../components/Content/Content';
+import ContentDownSyndrome from '../components/Content/ContentDownSyndrome';
+import HeroBanner from '../components/HeroBanner/HeroBanner';
+import Badge from '../components/Badge/Badge';
+import ContentBanner from '../components/ContentBanner/ContentBanner';
+import Faq from '../components/Faq/Faq';
+import Testimonials from '../components/Testimonials/Testimonials';
+import Contact from '../components/Contact/Contact';
+import Footer from '../components/Footer/Footer';
+import Video from '../components/Video/Video';
+import Pricing from '../components/Pricing/Pricing';
+
+const useStyles = makeStyles((theme) => ({
+  mainDiv: {},
+  testimonials: {
+    marginTop: theme.spacing(15),
+  },
+  heading: {
+    textAlign: 'center',
+    color: '#00b0f0',
+    fontWeight: 600,
+  },
+  subHeading: {
+    textAlign: 'center',
+  },
+}));
+
+export default function Home() {
+  const classes = useStyles();
+
+  return (
+    <main>
+      <Head>
+        <title>annspech.com</title>
+
+        <meta name="description" content="speech therapy" />
+        <meta name="keywords" content="speech therapy" />
+      </Head>
+
+      <Container className={classes.mainDiv} maxWidth="lg">
+        <Menu />
+
+        <Box spacing={3} className={classes.mainDiv}>
+          <Typography
+            className={classes.heading}
+            variant="h3"
+            component="div"
+            gutterBottom
+          >
+            Looking for a Speech Therapist for Down Syndrome?
+          </Typography>
+
+          <Typography
+            className={classes.subHeading}
+            variant="h6"
+            component="div"
+            gutterBottom
+          >
+            I handle conditions such as Speech Delay, Stuttering, Cleft Lip &
+            Palate, Unclear Speech, Reading & Writing difficulties and other
+            communication difficulties in Children and Adults!
+          </Typography>
+        </Box>
+        <Grid container spacing={2}>
+          <HeroBanner />
+
+          <Form />
+        </Grid>
+        <ContentDownSyndrome />
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={4}>
+            <ContentBanner />
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <Badge />
+            <Video />
+          </Grid>
+        </Grid>
+        <Faq />
+
+        <Testimonials />
+
+        <Contact />
+
+        <Footer />
+      </Container>
+    </main>
+  );
+}
