bcd6c0e6538c833f4dc3092fee2f87c6fb175584
iogrids <iogrids1@gmail.com>
2021-11-08 10:18:53 +0530

    updates

---
+++ src/components/Content/SpeechTherapyKids.js
@@ -0,0 +1,66 @@
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { makeStyles } from '@material-ui/core';
+import Container from '@mui/material/Container';
+
+const useStyles = makeStyles((theme) => ({
+  mainDiv: {
+    marginBottom: theme.spacing(3),
+  },
+  title: {
+    textAlign: 'center',
+    color: '#00b0f0',
+    fontWeight: 600,
+  },
+}));
+
+const Content = () => {
+  const classes = useStyles();
+  return (
+    <Box spacing={3} className={classes.mainDiv}>
+      <Typography
+        className={classes.title}
+        variant="h3"
+        component="div"
+        gutterBottom
+      >
+        Why should you seek help at the earliest?
+      </Typography>
+
+      <Grid container direction="row" alignItems="center">
+        <Typography variant="body1" gutterBottom>
+          It is very important that your child&apos;s communication difficulties
+          are identified and treated at the earliest. Younger the age, better
+          the positive outcomes from the treatment. This is because the human
+          brain is developing at a much faster rate during the first 3 years of
+          life and slows down after 6 years of age. During these initial years
+          of life, the brain has the maximum capacity to adapt and grow. Hence,
+          any intervention provided during these initial years will give maximum
+          benefits.
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          It is common for toddlers and young children to have SPEECH AND
+          LANGUAGE DELAY, also associated with conditions such as AUTISM,
+          ATTENTION DEFICIT HYPERACTIVITY DISORDER, CHILDHOOD APRAXIA OF SPEECH,
+          DOWN&apos;S SYNDROME, CEREBRAL PALSY, HEARING LOSS, CLEFT LIP AND
+          PALATE etc. Reading and writing disorders associated with LEARNING
+          DISABILITY, childhood STUTTERING, UNCLEAR SPEECH, ORO MOTOR AND
+          FEEDING ISSUES are also common.
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          As a Speech Language Pathologist I will provide a complete assessment
+          to know if your child has any of the conditions listed above, identify
+          the reasons for the issues present and provide you with a treatment
+          plan to improve your child&apos;s speech, language and feeding skills.
+          I will also answer all your concerns regarding your child&apos;s
+          speech, language and feeding issues and give you strategies that has
+          worked tremendously well for the kids I have given speech therapy in
+          various established speech therapy centres.
+        </Typography>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Content;
---
+++ src/pages/lp/online-speech-therapy-kids.js
@@ -0,0 +1,98 @@
+import Head from 'next/head';
+import Grid from '@mui/material/Grid';
+import { makeStyles } from '@material-ui/core';
+import Container from '@mui/material/Container';
+import Menu from '../../components/Menu/Menu';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Form from '../../components/Form/Form';
+import SpeechTherapyKids from '../../components/Content/SpeechTherapyKids';
+import HeroBanner from '../../components/HeroBanner/HeroBanner';
+import Badge from '../../components/Badge/Badge';
+import ContentBanner from '../../components/ContentBanner/ContentBanner';
+import FaqKids from '../../components/Faq/FaqKids';
+import Testimonials from '../../components/Testimonials/Testimonials';
+import Contact from '../../components/Contact/Contact';
+import Footer from '../../components/Footer/Footer';
+import Video from '../../components/Video/Video';
+
+const useStyles = makeStyles((theme) => ({
+  mainDiv: {},
+  testimonials: {
+    marginTop: theme.spacing(15),
+  },
+  heading: {
+    textAlign: 'center',
+    color: '#00b0f0',
+    fontWeight: 600,
+  },
+  subHeading: {
+    textAlign: 'center',
+  },
+}));
+
+export default function Home() {
+  const classes = useStyles();
+
+  return (
+    <main>
+      <Head>
+        <title>annspech.com</title>
+
+        <meta name="description" content="online speech therapy children" />
+        <meta
+          name="keywords"
+          content="online speech therapy, children, kids, child"
+        />
+      </Head>
+
+      <Container className={classes.mainDiv} maxWidth="lg">
+        <Menu />
+
+        <Box spacing={3} className={classes.mainDiv}>
+          <Typography
+            className={classes.heading}
+            variant="h3"
+            component="div"
+            gutterBottom
+          >
+            Over 10+ Yrs Experience Handling Online Speech Therapy for Kids!!
+          </Typography>
+
+          <Typography
+            className={classes.subHeading}
+            variant="h6"
+            component="div"
+            gutterBottom
+          >
+            I handle conditions such as Speech Delay, Stuttering, Cleft Lip &
+            Palate, Unclear Speech, Reading & Writing difficulties and other
+            communication difficulties for kids!!
+          </Typography>
+        </Box>
+        <Grid container spacing={2}>
+          <HeroBanner />
+
+          <Form />
+        </Grid>
+        <SpeechTherapyKids />
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={4}>
+            <ContentBanner />
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <Badge />
+            <Video />
+          </Grid>
+        </Grid>
+        <FaqKids />
+
+        <Testimonials />
+
+        <Contact />
+
+        <Footer />
+      </Container>
+    </main>
+  );
+}
